70c14ffb53298ae3e8fa86f67dc7275ce7fd1818
jspall0327 <jspall98@aol.com>
2019-08-27 16:14:16 -0400

    Initial motor control firmware

--- elegoo.h
+++ elegoo.h
@@ -1,5 +1,19 @@
+#define RIGHT_MOTORS 6
+#define LEFT_MOTORS 5
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+
 class Elegoo
 {
-
-
+	public:
+		Elegoo();
+		// Motor Control
+		motorForward(float pwrLeft, float pwrRight);
+		motorBackward(float pwrLeft, float pwrRight);
+		motorLeft(float pwrLeft, float pwrRight);
+		motorRight(float pwrLeft, float pwrRight);
+		motorStop();
 }
