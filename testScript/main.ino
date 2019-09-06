450d9e0f22ee7665b001a57dcc02eee1b3b53b8a
nehaloonie <34704834+nehaloonie@users.noreply.github.com>
2019-09-06 13:52:57 -0400

    Update main.ino

--- testScript/main.ino
+++ testScript/main.ino
@@ -9,42 +9,60 @@
 #include <Elegoo.h>
 
 // Main State Functions
-
 void followLine() {
 	// Check infrared sensors to determine position on the line
 	// Turn left or right according or move forward as appropriate
 	// Check obstacles consistently here or in main? 
+	if (isMiddleDark() && !isRightDark() && !isLeftDark()) {
+		// go straight
+	} else if (isMiddleDark() && isRightDark() && !isLeftDark()) {
+		// turn right
+		turnRight(); 
+	} else if (isMiddleDark() && !isRightDark() && isLeftDark()) {
+		// turn left
+		turnLeft(); 
+	} else if (isMiddleDark() && isRightDark() && isRightDark()) {
+		// you're at a station
+		waitAtStation(); 
+	}
 
 
 
 }
 
 void avoidObstacle() {
-
-	// If obstacle detected moves into here 
-	// Call stop motors until ultrasonic sensor no longer detects object
+	// If there is an object ahead
+	while (Elegoo::getDistance() < 10) {
+		// call stop
+		Elegoo::stopMotor(); 
+	}
+	// Once removed, return to following the line
+	followLine(); 
 
 }
 
 void waitAtStation() {
-
+	Elegoo::stopMotor(); 
 	// Once all three infrared sensors are lit, we enter here
 	// call check maintenance 
-	
-	if (checkMaintenance() == false) {
+	if (isObjectToRight() == false) {
 		// wait for one second
+		wait(); 
 	} else {
 		// call wait for maintenance
+		waitForMaintenance(); 
 	}
-
+	followLine(); 
 }
 
-bool waitForMaintenance() {
-
+void waitForMaintenance() {
+	while (isObjectToRight() == true) {
+		Elegoo::stopMotor(); 
+	}
+	followLine(); 
 }
 
 // Lower Level Functions required for Main States
-
 void turnLeft() {
 
 }
@@ -53,20 +71,44 @@ void turnRight() {
 
 }
 
-void stop() {
-
+void wait() {
+	// Wait one second
+	int t = 0; 
+	while (t < 100) {
+		Elegoo::stopMotor(); 
+		t = t+1; 
+	}
 }
 
 void isObjectAhead() {
+	Elegoo::setCenterServo(0); 
+	if (Elegoo::getDistance() < 10) {
+		Elegoo::stopMotor(); 
+		avoidObstacle(); 
+	}
+}
 
+bool isObjectToRight() {
+	// Set the servo to 90 degrees to the right
+	Elegoo::setPosServo(char 90); 
+	// Check for object
+	int dist = Elegoo::getDistance();
+	if (dist < 10) {
+		// There's maintenance happening
+		return true; 
+	} else {
+		// Maintenance is complete
+		// Move ultrasonic sensor into central position
+		Elegoo::setCenterServo(0);
+		return false; 
+	}
 
 }
 
-void isObjectToRight() {
+main() {
+	// Switch statement to switch between states
 
-}
 
-bool checkMaintenance() {
 
 }
 
