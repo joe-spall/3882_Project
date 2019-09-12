c2cae8737fea725c8ad7ce146d24fc3654cbfa98
vamsatoor <54722178+vamsatoor@users.noreply.github.com>
2019-09-12 15:09:20 -0400

    Created main2

---
+++ main2/elegoo.cpp
@@ -0,0 +1,230 @@
+#include "elegoo.h"
+
+Elegoo::Elegoo(){
+
+}
+
+void Elegoo::init(){
+        // Motor init
+    pinMode(IN1_PIN, OUTPUT);
+    pinMode(IN2_PIN, OUTPUT);
+    pinMode(IN3_PIN, OUTPUT);
+    pinMode(IN4_PIN, OUTPUT);
+    pinMode(LEFT_MOTORS_PIN, OUTPUT);
+    pinMode(RIGHT_MOTORS_PIN, OUTPUT);
+    stopMotor();
+    // Ultrasonic init
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(TRIG_PIN, OUTPUT);
+    // Ultrasonic servo init
+    pinMode(SERVO_PIN, OUTPUT);
+    ultrasonicServo.attach(SERVO_PIN);
+    goCenterServo();
+    // Light sensor init
+    pinMode(LIGHT_RIGHT_PIN,INPUT);
+    pinMode(LIGHT_CENTER_PIN,INPUT);
+    pinMode(LIGHT_LEFT_PIN,INPUT);
+
+    irrecv = IRrecv(RECV_REMOTE_PIN);
+    irrecv.enableIRIn();
+
+}
+
+// Motor Commands
+
+void Elegoo::goForwardMotor(float pwrLeft, float pwrRight){
+    if(verifyPwrMotor(pwrLeft,pwrRight)){
+        digitalWrite(IN1_PIN, HIGH);
+        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(IN3_PIN, LOW);
+        digitalWrite(IN4_PIN, HIGH);
+        writeSpeedMotor(pwrLeft,pwrRight);
+    } else{
+        Serial.println("Error: Cannot move forward");
+    }
+}
+
+void Elegoo::goBackwardMotor(float pwrLeft, float pwrRight){
+    if(verifyPwrMotor(pwrLeft,pwrRight)){
+        digitalWrite(IN1_PIN, LOW);
+        digitalWrite(IN2_PIN, HIGH);
+        digitalWrite(IN3_PIN, HIGH);
+        digitalWrite(IN4_PIN, LOW);
+        writeSpeedMotor(pwrLeft,pwrRight);
+    } else{
+        Serial.println("Error: Cannot move backward");
+    }
+}
+
+void Elegoo::goLeftMotor(float pwrLeft, float pwrRight){
+    if(verifyPwrMotor(pwrLeft,pwrRight)){
+        digitalWrite(IN1_PIN, LOW);
+        digitalWrite(IN2_PIN, HIGH);
+        digitalWrite(IN3_PIN, LOW);
+        digitalWrite(IN4_PIN, HIGH); 
+        writeSpeedMotor(pwrLeft,pwrRight);
+    } else{
+        Serial.println("Error: Cannot move left");
+    }
+}
+
+void Elegoo::goRightMotor(float pwrLeft, float pwrRight){
+    if(verifyPwrMotor(pwrLeft,pwrRight)){
+        digitalWrite(IN1_PIN, HIGH);
+        digitalWrite(IN2_PIN, LOW);
+        digitalWrite(IN3_PIN, HIGH);
+        digitalWrite(IN4_PIN, LOW);
+        writeSpeedMotor(pwrLeft,pwrRight);
+    } else{
+        Serial.println("Error: Cannot move right");
+    }
+}
+
+void Elegoo::stopMotor(){
+    digitalWrite(LEFT_MOTORS_PIN,0);
+    digitalWrite(RIGHT_MOTORS_PIN,0);
+}
+
+// pwrLeft, pwrRight in precentage from 0 to 1
+bool Elegoo::verifyPwrMotor(float pwrLeft, float pwrRight){
+    bool motorPwrValid = true;
+    if(pwrLeft > 1){
+        motorPwrValid = false;
+        Serial.println("Error: Left Motor Power exceeded 1");
+    }
+
+    if(pwrRight > 1){
+        motorPwrValid = false;
+        Serial.println("Error: Right Motor Power exceeded 1");
+    }
+
+    if(pwrLeft < 0){
+        motorPwrValid = false;
+        Serial.println("Error: Left Motor Power under 0");
+    }
+
+    if(pwrRight < 0){
+        motorPwrValid = false;
+        Serial.println("Error: Right Motor Power under 0");
+    }
+
+    return motorPwrValid;
+}
+
+void Elegoo::writeSpeedMotor(float pwrLeft, float pwrRight){
+    int leftWriteValue = pwrLeft * 255;
+    int rightWriteValue = pwrRight * 255;
+    analogWrite(LEFT_MOTORS_PIN, leftWriteValue);
+    analogWrite(RIGHT_MOTORS_PIN, rightWriteValue);
+}
+
+// Ultrasonic Commands
+
+// Ultrasonic distance measurement
+unsigned long Elegoo::getDistance() {
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    unsigned long distance = pulseIn(ECHO_PIN, HIGH, 5800) / 58;
+    if(distance == 0){
+    	return 150;
+    }
+    return distance;
+}
+
+
+// Ultrasonic Servo Commands
+
+// Ultrasonic Servo Centering Commands
+byte Elegoo::getCenterServo() {
+    return centerPosServo;
+}
+
+void Elegoo::setCenterServo(byte centerAngle) {
+    centerPosServo = centerAngle;
+}
+
+void Elegoo::goCenterServo() {
+    setPosServo(getCenterServo());
+}
+
+// Ultrasonic Servo Position commands
+byte Elegoo::getPosServo() {
+    return currPosServo;
+}
+
+void Elegoo::setPosServo(byte angle) {
+    if(verifyPosServo(angle)){
+        currPosServo = angle;
+        ultrasonicServo.write(currPosServo);
+    } else{
+        Serial.println("Error: Cannot move Ultrasonic Servo");
+    }
+    
+}
+
+// Angle in degrees from 0 to 180
+bool Elegoo::verifyPosServo(byte angle) {
+    bool servoPosValid = true;
+    if(angle < 0){
+        servoPosValid = false;
+        Serial.println("Error: Servo angle under 0");
+    }
+    if(angle > 180){
+        servoPosValid = false;
+        Serial.println("Error: Servo angle over 180");
+    }
+    return servoPosValid;
+}
+
+// Light Sensor Commands
+bool Elegoo::isRightDark(){
+    return !digitalRead(LIGHT_RIGHT_PIN);
+}
+
+bool Elegoo::isCenterDark(){
+    return !digitalRead(LIGHT_CENTER_PIN);
+}
+
+bool Elegoo::isLeftDark(){
+    return !digitalRead(LIGHT_LEFT_PIN);
+}
+
+// IR Remote Commands
+void Elegoo::setStateRemote(){
+    if (irrecv.decode(&resultsRemote)){ 
+        unsigned long valueRemote = resultsRemote.value;
+        irrecv.resume();
+        switch(valueRemote){
+            case FORWARD_CODE:{
+                stateRemote = FORWARD;
+                break;
+            }
+            case BACKWARD_CODE:{
+                stateRemote = BACKWARD;
+                break;
+            }
+            case LEFT_CODE:{
+                stateRemote = LEFT;
+                break;
+            }
+            case RIGHT_CODE:{
+                stateRemote = RIGHT;
+                break;
+            }
+            case STOP_CODE:{
+                stateRemote = STOP;
+            	break;
+            }
+            default:{
+            	break;
+            }
+        }
+    }
+}
+
+IR_REMOTE_STATE Elegoo::getStateRemote(){
+    return stateRemote;
+}
---
+++ main2/main2.ino
@@ -0,0 +1,279 @@
+/**
+ * ECE 3882
+ * GROUP NUMBER: 15
+ * MEMBERS: JOE SPALL, VANSISH SATOOR, ERIC PEARSON, NEHA PASRICHA
+ * Main code base to run the line following robot using appropriate 
+ * software design techniques
+ */
+
+#include "Elegoo.h"
+
+#define RIGHT_POS 180
+#define CENTER_POS 90 
+#define MIN_OBS_DIST 10
+#define MIN_STATION_DIST 10
+#define FORWARD_POWER_LM 0.25
+#define FORWARD_POWER_RM 0.25
+#define RFORWARD_POWER_LM 0.25
+#define RFORWARD_POWER_RM 0.25
+#define LFORWARD_POWER_LM 0.25
+#define LFORWARD_POWER_RM 0.25
+#define HRFORWARD_POWER_RM 0.5
+#define HRFORWARD_POWER_LM 0.5
+#define HLFORWARD_POWER_LM 0.5
+#define HLFORWARD_POWER_RM 0.5
+#define LEFT_CHECK_CYCLES 5
+#define RIGHT_CHECK_CYCLES 10
+
+
+enum ROBOT_STATE{
+	FOLLOW,
+	AVOID,
+	STATION,
+	MANUFACTURING,
+	IDLE
+};
+
+Elegoo robot;
+ROBOT_STATE currentState;
+bool finishedStop = false; 
+bool finishedStation = false; 
+bool finishedManufacturing = false;
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+    robot.init();
+    robot.setCenterServo(CENTER_POS); 
+    currentState = IDLE;
+
+}
+
+void loop() {
+    Serial.println("Go");
+  // put your main code here, to run repeatedly:
+    determineState(); 
+    runState(); 
+
+    Serial.println("Left Line Sensor:");
+    Serial.println(robot.isLeftDark());
+    Serial.println("Middle Line Sensor:");
+    Serial.println(robot.isCenterDark());
+    Serial.println("Left Line Sensor:");
+    Serial.println(robot.isLeftDark());
+    Serial.println("Current State:");
+    Serial.println(currentState);
+    Serial.println("Sensor Distance:");
+    Serial.println(robot.getDistance());
+
+}
+
+// Determine what state you're in 
+void determineState() {
+	switch (currentState) {
+		case FOLLOW: {
+			if (isObjectAhead()) {
+				currentState = AVOID; 
+			}
+			if (isAtStation()) {
+				currentState = STATION;
+			}
+			break; 
+		}			
+		case AVOID: {
+			if (!isObjectAhead() && finishedStop) {
+				currentState = FOLLOW; 
+				finishedStop = false; 
+			}
+			break;
+		}
+		case STATION: {
+			if (isObjectToRight()) {
+				currentState = MANUFACTURING;
+			} else if (finishedStation) {
+				currentState = FOLLOW; 
+				finishedStation = false; 
+			}
+			break; 
+		}
+		case MANUFACTURING: {
+			if (!isObjectToRight() && finishedManufacturing) {
+				currentState = FOLLOW; 
+			} 
+			break; 
+		}
+		case IDLE: {
+			currentState = FOLLOW; 
+			break; 
+		}
+	}
+}
+
+// Run the current state
+void runState() {
+	switch (currentState) {
+		case FOLLOW: {
+			followLine(); 
+			break; 
+		}
+		case AVOID: {
+			avoidObstacle(); 
+			break; 
+		}
+		case STATION: {
+			waitAtStation(); 
+			break; 
+		}
+		case MANUFACTURING: {
+			waitForManufacturing(); 
+			break; 
+		}
+		case IDLE: {
+			wait(); 
+			break; 
+		}
+	}
+}
+
+// Main State Functions
+void followLine() {
+	// Check infrared sensors to determine position on the line
+	// Turn left or right according or move forward as appropriate
+	// Check obstacles consistently here or in main? 
+	if (robot.isCenterDark() && !robot.isRightDark() && !robot.isLeftDark()) {
+		// go straight
+		goStraight(); 
+	} else if (robot.isCenterDark() && robot.isRightDark() && !robot.isLeftDark()) {
+		// turn right
+		turnRight(); 
+	} else if (robot.isCenterDark() && !robot.isRightDark() && robot.isLeftDark()) {
+		// turn left
+		turnLeft(); 
+	} else if (robot.isCenterDark() && robot.isRightDark() && robot.isLeftDark()) {
+		// At station
+		robot.stopMotor(); 
+	} else if (!robot.isCenterDark() && robot.isRightDark() && !robot.isLeftDark()) {
+		// turn more right
+		turnHardRight(); 
+	} else if (!robot.isCenterDark() && !robot.isRightDark() && robot.isLeftDark()) {
+		// turn more left
+		turnHardLeft(); 
+	} else if (!robot.isCenterDark() && !robot.isRightDark() && !robot.isLeftDark()) {
+		// turn more right
+		findLine(); 
+	} 
+
+}
+
+void avoidObstacle() {
+	robot.goCenterServo(); 
+	// If there is an object ahead
+	while (robot.getDistance() < MIN_OBS_DIST) {
+		// call stop
+		robot.stopMotor(); 
+	}
+	finishedStop = true; 
+}
+
+void waitAtStation() {
+	robot.stopMotor(); 
+	wait(); 
+	finishedStation = true; 
+}
+
+void waitForManufacturing() {
+	while (isObjectToRight()) {
+		wait(); 
+	}
+	robot.goCenterServo(); 
+	wait(); 
+	finishedManufacturing = true; 
+}
+
+// Lower Level Functions required for Main States
+bool isAtStation() {
+	if (robot.isCenterDark() && robot.isRightDark() && robot.isRightDark()) {
+		robot.stopMotor(); 
+		return true; 
+	} else {
+		return false; 
+	}
+}
+
+void goStraight() {
+	robot.goForwardMotor(FORWARD_POWER_LM,FORWARD_POWER_RM);
+}
+
+void turnLeft() {
+	robot.goLeftMotor(LFORWARD_POWER_LM,LFORWARD_POWER_RM);
+}
+
+void turnRight() {
+	robot.goRightMotor(RFORWARD_POWER_LM,RFORWARD_POWER_RM);
+}
+
+void turnHardLeft() {
+	robot.goLeftMotor(HLFORWARD_POWER_LM,HLFORWARD_POWER_RM);
+}
+
+void turnHardRight() {
+	robot.goRightMotor(HRFORWARD_POWER_LM,HRFORWARD_POWER_RM);
+}
+
+void findLine() {
+	bool noLineFound = true; 
+	int leftCount = 0; 
+	int rightCount = 0; 
+	while(noLineFound && leftCount < LEFT_CHECK_CYCLES) {
+		robot.goLeftMotor(LFORWARD_POWER_LM,LFORWARD_POWER_RM);
+		if (robot.isCenterDark() || robot.isRightDark() || robot.isLeftDark()) {
+			noLineFound = false; 
+		}
+		delay(100);
+		leftCount++; 
+	}
+	while (noLineFound && rightCount < RIGHT_CHECK_CYCLES) {
+		robot.goRightMotor(RFORWARD_POWER_LM,RFORWARD_POWER_RM); 
+		if (robot.isCenterDark() || robot.isRightDark() || robot.isLeftDark()) {
+			noLineFound = false; 
+		}
+		delay(100);
+		rightCount++; 
+	}
+
+}
+
+void wait() {
+	// Wait one second
+	robot.stopMotor(); 
+	delay(1000); 
+}
+
+bool isObjectAhead() {
+	robot.goCenterServo(); 
+	if (robot.getDistance() < MIN_OBS_DIST) {
+		robot.stopMotor(); 
+		return true; 
+	} 
+	return false; 
+}
+
+bool isObjectToRight() {
+	// Set the servo to 180 degrees to the right
+	robot.goCenterServo(); 
+	// Check for object
+	if (robot.getDistance() < MIN_STATION_DIST) {
+		// There's manufacturing happening
+		return true; 
+	} else {
+		// Manufacturing is complete
+		// Move ultrasonic sensor into central position
+		robot.goCenterServo();
+		return false; 
+	}
+
+}
+
+
+
+
